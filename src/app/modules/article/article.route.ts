038185f7bd0a19fc813f4eb675addd7e83da0126
unknown <alaminatghoria@gmail.com>
2024-11-18 12:44:30 +0600

    auth added on article update

--- src/app/modules/article/article.route.ts
+++ src/app/modules/article/article.route.ts
@@ -1,11 +1,23 @@
 import express from "express";
 import { articleController } from "./article.controller";
+import auth from "../../middlewares/auth";
 
 const router = express.Router();
 
-router.post("/create", articleController.createArticle); //create
+//create
+router.post(
+  "/create",
+  auth("admin", "editor"),
+  articleController.createArticle
+);
 router.get("/articles", articleController.getAllArticles); //get all articles
-router.patch("/update/:id", articleController.updateArticle); //update articles
+
+//update articles
+router.patch(
+  "/update/:id",
+  auth("admin", "editor"),
+  articleController.updateArticle
+);
 router.get("/breaking", articleController.getBreakings); //get breakings
 router.get("/featured", articleController.getFeatured); //get featured
 router.get("/tending", articleController.getTending); //get tending
